4e1cd445841e4fef93c7b737e3da354aaf3a2469
theody85 <gyambrahtheodora.8@gmail.com>
2022-07-13 11:52:31 +0000

    Add file for task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Class Module"""
+
+
+class Student:
+    """defines a student by certain qualities"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiates first_name, last_name and age of an instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        slef.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
+        return(self.__dict)
