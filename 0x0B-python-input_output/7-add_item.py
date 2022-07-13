0a7ca1e53cdf8d459c2b61547c032eac192dc82b
theody85 <gyambrahtheodora.8@gmail.com>
2022-07-13 11:34:42 +0000

    Update file for task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"Input and output module"
+"Method Module"
+import json
 from sys import argv
 
 
-
-save_to_json_file = __import('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-
+# Create the object or calling from json
 try:
-    with open(filename, 'r') as f:
-        obj = load_from_json_file(f)
+    f = open(filename, 'r')
+    obj = load_from_json_file(filename)
 except Exception:
     obj = []
 
 for i in range(1, len(argv)):
     obj.append(argv[i])
 
+# Create json file
 with open(filename, 'w') as f:
-    save_to_json_file(obj, f)
+    json.dump(obj, f)
