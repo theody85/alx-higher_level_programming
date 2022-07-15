f025694cc85b37aa065f0f0ec14290eb74f31815
theody85 <gyambrahtheodora.8@gmail.com>
2022-07-15 02:25:13 +0000

    Update file for task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -7,13 +7,11 @@ class Student:
     """defines a student by certain qualities"""
 
     def __init__(self, first_name, last_name, age):
-        """Instantiates first_name, last_name and age of an instance"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of a Student instance"""
         if attrs is not None:
             li = {}
             for a in attrs:
