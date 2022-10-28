b8ae175ee1f426223be0ff7e6bc813b2b3438097
Theodora Addeafi Gyambrah <70439774+theody85@users.noreply.github.com>
2022-10-28 00:57:00 +0000

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"Method Module"
+"""Module 7-add_item.
+Adds all arguments to a Python list,
+and then save them to a file.
+"""
+
+import sys
 import json
-from sys import argv
+import os.path
 
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+my_file = 'add_item.json'
 
-filename = "add_item.json"
-# Create the object or calling from json
-try:
-    with open(filename, 'r') as f:
-        obj = load_from_json_file(f)
-except Exception:
-    obj = []
+my_list = []
 
-for i in range(1, len(argv)):
-    obj.append(argv[i])
+if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
+    my_list = load_from_json_file(my_file)
 
-# Create json file
-with open(filename, 'w') as f:
-    save_to_json_file(obj, f)
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        my_list.append(elem)
+
+save_to_json_file(my_list, my_file)
